7888f095c4fc831018104f4fc5e44c70d45adb42
ccHarvestasya <harvestasya84@gmail.com>
2024-08-17 22:14:44 +0900

    pm2がエラー拾えるように修正

--- src/rest/pm2Service.ts
+++ src/rest/pm2Service.ts
@@ -6,51 +6,45 @@ import { RestChain } from './RestChain.js'
 import { RestNode } from './RestNode.js'
 import { RestTransaction } from './RestTransaction.js'
 
-class Pm2Service {
-  start() {
-    /** ロガー */
-    const logger = new Logger('rest')
+/** ロガー */
+const logger = new Logger('rest')
 
-    /** コマンドライン引数 */
-    const args = process.argv.slice(2)
+/** コマンドライン引数 */
+const args = process.argv.slice(2)
 
-    /** コマンドライン引数 */
-    // コンフィグファイル
-    const configFilePath = args[0]
-    logger.debug(`argConfigFilePath: ${configFilePath}`)
+/** コマンドライン引数 */
+// コンフィグファイル
+const configFilePath = args[0]
+logger.debug(`argConfigFilePath: ${configFilePath}`)
 
-    try {
-      /** コンフィグファイル */
-      const config = ConfigMgr.loadConfig(configFilePath)
+try {
+  /** コンフィグファイル */
+  const config = ConfigMgr.loadConfig(configFilePath)
 
-      /** Catapult */
-      const host = config.isDebug ? 'sakia.harvestasya.com' : '127.0.0.1'
-      logger.debug(`Catapult Server Connection Host: ${host}`)
-      const catapult = new Catapult(config.certPath, host, config.peerPort)
+  /** Catapult */
+  const host = config.isDebug ? 'sakia.harvestasya.com' : '127.0.0.1'
+  logger.debug(`Catapult Server Connection Host: ${host}`)
+  const catapult = new Catapult(config.certPath, host, config.peerPort)
 
-      /** サーバ設定 */
-      const server = createServer(async (request, response) => {
-        await new RestChain().response(request, response, catapult)
-        await new RestNode().response(request, response, catapult)
-        await new RestTransaction().response(request, response, catapult)
+  /** サーバ設定 */
+  const server = createServer(async (request, response) => {
+    await new RestChain().response(request, response, catapult)
+    await new RestNode().response(request, response, catapult)
+    await new RestTransaction().response(request, response, catapult)
 
-        if (!response.headersSent) {
-          // ヘッダ書き込みなしは404
-          const ip = request.headers['x-forwarded-for'] || request.socket.remoteAddress
-          logger.error(`${ip} 404 ${request.url}`)
-          response.writeHead(404, { 'Content-Type': 'application/json' })
-          response.end(`{"code":"ResourceNotFound","message":"${request.url} does not exist"}`)
-        }
-      })
-
-      /** サーバ開始 */
-      logger.debug(`Started REST service listening on port ${config.restPort}.`)
-      server.listen(config.restPort)
-    } catch (e) {
-      logger.error(e as string)
-      logger.shutdown(-1)
+    if (!response.headersSent) {
+      // ヘッダ書き込みなしは404
+      const ip = request.headers['x-forwarded-for'] || request.socket.remoteAddress
+      logger.error(`${ip} 404 ${request.url}`)
+      response.writeHead(404, { 'Content-Type': 'application/json' })
+      response.end(`{"code":"ResourceNotFound","message":"${request.url} does not exist"}`)
     }
-  }
-}
+  })
 
-new Pm2Service().start()
+  /** サーバ開始 */
+  logger.debug(`Started REST service listening on port ${config.restPort}.`)
+  server.listen(config.restPort)
+} catch (e) {
+  logger.error(e as string)
+  logger.shutdown(-1)
+}
